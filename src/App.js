1453318a61d4d05f16c874c11ac7f9d68bd01b2b
Sarthak Tiwari <sarthaktiwari1542@gmail.com>
2025-08-23 15:56:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import nextevent from './images/nextevent.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button className="notify">Notify Me</button> */}
+      <button className="kdj">Notify Me</button>
+      <h1 className="first-heading">Connect</h1>
+      <h1 className="second-heading">through</h1>
+      <h1 className="third heading">experiences</h1>
     </div>
   );
 }
